7876f0213a7ff7eb2b9501b550c92e4b94f0b1c2
unknown <you@example.com>
2025-09-08 10:56:36 +0200

    fix: Add dynamic edit routes to App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Layout from './components/Layout';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import ForgotPassword from './pages/ForgotPassword'; // Import the new component
+import ForgotPassword from './pages/ForgotPassword';
 import CustomerList from './pages/CustomerList';
 import CustomerForm from './pages/CustomerForm';
 import DealsList from './pages/DealsList';
@@ -16,9 +16,9 @@ import TaskList from './pages/TaskList';
 import TaskForm from './pages/TaskForm'; 
 import InventoryList from './pages/InventoryList';
 import InventoryForm from './pages/InventoryForm';
-import InvoiceList from './pages/InvoiceList'; // New Import
-import InvoiceForm from './pages/InvoiceForm'; // New Import
-import InvoiceDetails from './pages/InvoiceDetails'; // New Import
+import InvoiceList from './pages/InvoiceList';
+import InvoiceForm from './pages/InvoiceForm';
+import InvoiceDetails from './pages/InvoiceDetails';
 import PrivateRoute from './components/PrivateRoute';
 import ResetPassword from './pages/ResetPassword';
 import './App.css';
@@ -30,26 +30,29 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} /> {/* Add the new route */}
+          <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/reset-password/:token" element={<ResetPassword />} />
           <Route path="/" element={<PrivateRoute><Layout /></PrivateRoute>}>
             <Route index element={<Dashboard />} />
             <Route path="customers" element={<CustomerList />} />
             <Route path="customers/add" element={<CustomerForm />} />
+            <Route path="customers/edit/:id" element={<CustomerForm />} /> {/* Corrected path */}
             <Route path="deals" element={<DealsList />} />
             <Route path="deals/add" element={<DealForm />} />
+            <Route path="deals/edit/:id" element={<DealForm />} /> {/* Corrected path */}
             <Route path="employees" element={<EmployeeList />} /> 
             <Route path="employees/add" element={<EmployeeForm />} />
+            <Route path="employees/edit/:id" element={<EmployeeForm />} /> {/* Corrected path */}
             <Route path="tasks" element={<TaskList />} />
             <Route path="tasks/add" element={<TaskForm />} />
             <Route path="tasks/edit/:id" element={<TaskForm />} />
             <Route path="inventory" element={<InventoryList />} />
             <Route path="inventory/add" element={<InventoryForm />} />
             <Route path="inventory/edit/:id" element={<InventoryForm />} />
-            <Route path="invoices" element={<InvoiceList />} /> {/* New Invoices List route */}
-            <Route path="invoices/add" element={<InvoiceForm />} /> {/* New Invoice Add route */}
-            <Route path="invoices/edit/:id" element={<InvoiceForm />} /> {/* New Invoice Edit route */}
-            <Route path="invoices/:id" element={<InvoiceDetails />} /> {/* New Invoice Details route */}
+            <Route path="invoices" element={<InvoiceList />} />
+            <Route path="invoices/add" element={<InvoiceForm />} />
+            <Route path="invoices/edit/:id" element={<InvoiceForm />} />
+            <Route path="invoices/:id" element={<InvoiceDetails />} />
           </Route>
           <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
